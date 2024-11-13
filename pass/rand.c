40bd65c4fe8aff589ce036f7bc98137593c99883
Naveen Unnikrishnan <naveenunnikrishnan98@gmail.com>
2024-11-13 15:53:21 -0500

    Use OpenSSL's PRNG if RDRAND not available

--- pass/rand.c
+++ pass/rand.c
@@ -1,6 +1,8 @@
-#include <immintrin.h>
 #include <stdint.h>
 #include "rand.h"
+
+#ifdef __rdrand__
+#include <immintrin.h>
 
 uint16_t get_rand16() {
     uint16_t rand;
@@ -19,3 +21,33 @@ unsigned long long get_rand64() {
     while(!_rdrand64_step(&rand));
     return rand;
 }
+
+#else
+
+#include <openssl/rand.h>
+
+uint16_t get_rand16() {
+    uint16_t random_number;
+    if (RAND_bytes((unsigned char*)&random_number, sizeof(random_number)) != 1) {
+        return 0;
+    }
+    return random_number;
+}
+
+uint32_t get_rand32() {
+    uint32_t random_number;
+    if (RAND_bytes((unsigned char*)&random_number, sizeof(random_number)) != 1) {
+        return 0;
+    }
+    return random_number;
+}
+
+unsigned long long get_rand64() {
+    unsigned long long random_number;
+    if (RAND_bytes((unsigned char*)&random_number, sizeof(random_number)) != 1) {
+        return 0;
+    }
+    return random_number;
+}
+
+#endif
