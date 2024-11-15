4604de78854d81bd339601ce26001b919d830d5c
Naveen Unnikrishnan <naveenunnikrishnan98@gmail.com>
2024-11-14 22:39:20 -0500

    Update the pass to use a combined alloca with GetElementPtrs

--- pass/canarypass.cpp
+++ pass/canarypass.cpp
@@ -7,65 +7,141 @@ namespace llvm {
 
 struct CanaryPass : public PassInfoMixin<CanaryPass> {
 
-  Instruction *getSecondAlloca(BasicBlock &BB) {
-    int numAllocas = 0;
-    Instruction *secondAlloca = nullptr;
-
+  SmallVector<AllocaInst *> getAllocas(BasicBlock &BB) {
+    SmallVector<AllocaInst *> allocas;
     for (auto &I : BB) {
-      if (I.getOpcode() == Instruction::Alloca) {
-        if (numAllocas++ > 0) {
-          secondAlloca = &I;
-          break;
-        };
+      if (auto *AI = dyn_cast<AllocaInst>(&I)) {
+        allocas.push_back(AI);
       }
     }
-    return secondAlloca;
+    return allocas;
   }
+
+  /* -------------------------------------------------------------------------- */
+  /*                        Create a consolidated alloca                        */
+  /* -------------------------------------------------------------------------- */
+
+  AllocaInst *
+  createConsolidatedAlloca(Function &F,
+                           const SmallVector<AllocaInst *> &allocas) {
+    const DataLayout &dataLayout = F.getParent()->getDataLayout();
+    uint64_t totalSize = 0;
+    unsigned maxAlign = 1;
+
+    // Calculate total size and maximum alignment
+    for (AllocaInst *allocaInst : allocas) {
+      Type *allocaType = allocaInst->getAllocatedType();
+      unsigned alignment = allocaInst->getAlign().value();
+
+      // Align the offset
+      totalSize = (totalSize + alignment - 1) & ~(alignment - 1);
+      totalSize += dataLayout.getTypeAllocSize(allocaType);
+      totalSize += dataLayout.getTypeAllocSize(Type::getInt8Ty(F.getContext()));
+      maxAlign = std::max(maxAlign, alignment);
+    }
+
+    // Create new alloca instruction
+    IRBuilder<> Builder(&F.getEntryBlock().front());
+    errs() << "Allocating a block with total size: " << totalSize << "\n";
+    Type *i8ArrayTy =
+        ArrayType::get(Type::getInt8Ty(F.getContext()), totalSize);
+    AllocaInst *newAlloca =
+        Builder.CreateAlloca(i8ArrayTy, nullptr, "consolidated");
+    newAlloca->setAlignment(Align(maxAlign));
+
+    return newAlloca;
+  }
+
+  /* -------------------------------------------------------------------------- */
+  /*                     Replace allocas with GetElementPtr                     */
+  /* -------------------------------------------------------------------------- */
+
+  void replaceAllocaWithGEP(AllocaInst *oldAlloca,
+                            AllocaInst *consolidatedAlloca, Value *offset) {
+    IRBuilder<> Builder(oldAlloca);
+
+    // Create GEP instruction
+    Value *zero =
+        ConstantInt::get(Type::getInt64Ty(oldAlloca->getContext()), 0);
+
+    SmallVector<Value *> indices = {zero, offset};
+    Value *gep = Builder.CreateGEP(consolidatedAlloca->getAllocatedType(),
+                                   consolidatedAlloca, indices, "gep");
+
+    // Bitcast to original type if necessary
+    Value *replacement =
+        Builder.CreateBitCast(gep, oldAlloca->getType(), "cast");
+
+    // Replace all uses of the old alloca
+    oldAlloca->replaceAllUsesWith(replacement);
+  }
+
+  /* -------------------------------------------------------------------------- */
+  /*                         Pass to add random padding                         */
+  /* -------------------------------------------------------------------------- */
 
   PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM) {
     if (F.getName().str() == "main") {
       return PreservedAnalyses::all();
     }
 
-    BasicBlock &BB = F.front();
-    Instruction *secondAlloca = getSecondAlloca(BB);
+    BasicBlock &BB = F.getEntryBlock();
+    LLVMContext &ctx = F.getContext();
+    Type *i32 = IntegerType::getInt32Ty(ctx);
+    const DataLayout &dataLayout = F.getParent()->getDataLayout();
 
-    if (secondAlloca == nullptr) {
+    SmallVector<AllocaInst *> allocas = getAllocas(BB);
+
+    if (allocas.empty()) {
+      errs() << "No stack allocations found for " << F.getName().str() << "\n";
       return PreservedAnalyses::all();
     }
 
-    errs() << "Updating CFG for " << F.getName().str() << "\n";
+    errs() << "Updating stack allocations for " << F.getName().str() << "\n";
 
-    LLVMContext &ctx = F.getContext();
-    Type *i32 = IntegerType::getInt32Ty(ctx);
+    // Create a consolidated alloca instruction
+    AllocaInst *consolidatedAlloca = createConsolidatedAlloca(F, allocas);
 
-    BasicBlock *merge_block = BB.splitBasicBlock(secondAlloca);
-    BasicBlock *canary_block =
-        BasicBlock::Create(ctx, "canary", &F, merge_block);
-
-    // Remove the existing block terminator (unconditional branch)
-    BB.getTerminator()->eraseFromParent();
-
-    // Get a random number and branch to canary block if odd
-    IRBuilder<> builder(&BB);
+    // Get a random number
+    IRBuilder<> builder(allocas[0]);
 
     FunctionType *randType = FunctionType::get(i32, false);
     FunctionCallee randFunc =
         F.getParent()->getOrInsertFunction("get_rand32", randType);
-    Value *RandomVal = builder.CreateCall(randFunc);
-    Value *One = ConstantInt::get(i32, 1);
-    Value *RandomBit = builder.CreateAnd(RandomVal, One);
+    Value *randomVal = builder.CreateCall(randFunc);
 
-    Value *cmp = builder.CreateICmpEQ(RandomBit, One, "one_check");
-    builder.CreateCondBr(cmp, canary_block, merge_block);
+    Value *one = ConstantInt::get(i32, 1);
+    Value *remainingRandBits = randomVal;
+    Value *currentOffset = ConstantInt::get(i32, 0);
 
-    // Insert canary in the canary block
-    builder.SetInsertPoint(canary_block);
-    AllocaInst *canary = builder.CreateAlloca(i32, nullptr, "Foo");
-    StoreInst *store = builder.CreateStore(One, canary);
+    // Replace allocas with GetElementPtrs
+    for (AllocaInst *allocaInst : allocas) {
+      builder.SetInsertPoint(allocaInst);
+      Value *randomBit = builder.CreateAnd(remainingRandBits, one, "randomBit");
+      Value *alignment = ConstantInt::get(i32, allocaInst->getAlign().value());
+      Value *alignMask = builder.CreateSub(alignment, one, "alignMask");
 
-    // Add a terminator for the canary block
-    builder.CreateBr(merge_block);
+      // We do currentOffset = (currentOffset + alignment - 1) & ~(alignment - 1)
+      // to ensure the variable is aligned correctly
+      currentOffset =
+          builder.CreateAnd(builder.CreateAdd(currentOffset, alignMask),
+                            builder.CreateNot(alignMask), "alignedOffset");
+
+      replaceAllocaWithGEP(allocaInst, consolidatedAlloca, currentOffset);
+
+      Type *allocaType = allocaInst->getAllocatedType();
+      currentOffset = builder.CreateAdd(
+          currentOffset,
+          ConstantInt::get(i32, dataLayout.getTypeAllocSize(allocaType)));
+
+      currentOffset = builder.CreateAdd(currentOffset, randomBit);
+      remainingRandBits = builder.CreateLShr(remainingRandBits, one);
+    }
+
+    // Clean up old allocas
+    for (AllocaInst *allocaInst : allocas) {
+      allocaInst->eraseFromParent();
+    }
 
     return PreservedAnalyses::none();
   }
--- pass/rand.c
+++ pass/rand.c
@@ -1,5 +1,18 @@
+#include <limits.h>
 #include <stdint.h>
+#include <stdio.h>
 #include "rand.h"
+
+void binprintf(uint32_t v)
+{
+    unsigned int mask = 1 << (sizeof(uint32_t) * CHAR_BIT  - 1);
+    printf("Generated random number: ");
+    while(mask) {
+        printf("%d", (v&mask ? 1 : 0));
+        mask >>= 1;
+    }
+    printf("\n\n");
+}
 
 #ifdef __rdrand__
 #include <immintrin.h>
@@ -13,6 +26,7 @@ uint16_t get_rand16() {
 uint32_t get_rand32() {
     uint32_t rand;
     while(!_rdrand32_step(&rand));
+    binprintf(rand);
     return rand;
 }
 
